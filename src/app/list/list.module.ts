5567c104447f106e04181740901d6f30390e695d
MarkAlexI <markovaleksandr108@gmail.com>
2023-08-09 11:37:10 +0300

    Added directive for highlighting selected rows of table.

--- src/app/list/list.module.ts
+++ src/app/list/list.module.ts
@@ -2,11 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { PurchaseListComponent } from './list.component';
+import { ActiveDirective } from '../directive/active.directive';
 
 @NgModule({
-  imports: [ BrowserModule, FormsModule ],
-  declarations: [ PurchaseListComponent],
-  exports: [ PurchaseListComponent]
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  declarations: [
+    PurchaseListComponent,
+    ActiveDirective
+  ],
+  exports: [PurchaseListComponent]
 })
 
 export class PurchaseListModule { }
